4697c56e71edc656ec9ada27c35ba145dd77b9c4
Dnld <steinert.donald@gmail.com>
2015-09-03 19:51:12 -0400

    refactors click event in player view

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -5,10 +5,12 @@ var PlayerView = Backbone.View.extend({
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
   el: '<audio controls autoplay />',
 
-  initialize: function() {
-    this.$el.on('ended', function() {
+  events: {
+    'ended': function() {
       this.model.ended();
-    }.bind(this));
+    }
+    // Why doesn't this work?
+    // 'ended': this.model.ended.bind(this)
   },
 
   setSong: function(song){
