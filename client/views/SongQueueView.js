968ab552855630bf7f548fe4584cbdd19ab36e9e
Dnld <steinert.donald@gmail.com>
2015-09-03 21:57:21 -0400

    removes console.log statement

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -6,7 +6,6 @@ var SongQueueView = Backbone.View.extend({
   initialize: function() {
     this.render();
     this.collection.on('add remove', function() {
-      console.log('SongQueueView listener');
       this.render();
     }, this);
   },
